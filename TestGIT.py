d551362f6ff5c9772690f02bbb9f90d04483ec49
daudfauzy98 <daudfauzy98@gmail.com>
2020-10-01 17:34:14 +0700

    Mengupdate file TestGIT.py dan menambahkan file TestGIT-2.py

--- TestGIT.py
+++ TestGIT.py
@@ -1,4 +1,8 @@
-# Skrip ini berisi source code untuk mengetes Code Versioning dengan GIT
+# Skrip ini berisi source code untuk mencoba Code Versioning dengan GIT
 
 
 print("Belajar Implementasi GIT pada VS Code")
+
+print("Pesan : ", end='')
+for _ in "Belajar GIT":
+    print(_, end='')
